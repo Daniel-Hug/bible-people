dd7fe94e5daee72cf70422f0cbdc6ab2f08a7518
Daniel Hug <daniel@hugwebdesign.com>
2017-11-10 16:54:22 -0800

    OOP

--- app.js
+++ app.js
@@ -1,69 +1,24 @@
-var handlePeople = (function() {
-  var treeParent = document.getElementById('tree');
-
-  return function handlePeople(data) {
-    window.app = data;
-    var people = app.people;
-
-    addParents(people);
-    // graphTree(getGraphData(people))
-
-    // if ( (personKey = getRoute()) ) {
-    //   displayCard(personKey);
-    // }
-
-    setupRoutes();
-  };
-})();
-
-
-/*ajax.get({
-  url: 'people.json',
-  success: handlePeople
-});*/
-initReftagger();
-handlePeople(peopleData);
-
-function setupRoutes() {
-  var root = null;
-  var useHash = true;
-  var router = new Navigo(root, useHash);
-
-  router
-  .on('*', function (params) {
-    var hash = location.hash.slice(1);
-    var personKey = hash.slice(1);
-    if (personKey && app.people[personKey]) {
-      displayCard(personKey);
-      document.title = getName(personKey) + ' in the Bible';
-      if (refTagger.tag) refTagger.tag();
-    } else if (!personKey) {
-      location.hash = '#!adam';
-    } else {
-      console.error('Invalid person key: ' + personKey + '.');
-    }
-  })
-  .resolve();
-}
-
-
 
 
 /*
   helper function declarations
 */
 
-function displayCard(personKey) {
+function Card(personKey) {
+  var card = this;
+  this.personKey = personKey;
+
   var cardParent = document.querySelector('#card-parent');
   removeChilds(cardParent);
   dom({
     el: cardParent,
-    kids: [renderCard(personKey)]
+    kids: [card.render()]
   });
 }
 
-function renderCard(personKey) {
+Card.prototype.render = function renderCard() {
   var people = app.people;
+  var personKey = this.personKey;
   var person = people[personKey];
   var references = person.references;
 
@@ -73,13 +28,13 @@ function renderCard(personKey) {
 
   if (person.yearsLived) detailItems.push({ el: 'li', kids: [
     'Lived ' + person.yearsLived + ' years',
-    renderReference(references.yearsLived)
+    this.renderReference(references.yearsLived)
   ]});
 
   var father = { el: 'li', kids: [
     { el: 'strong', text: 'Father:' }, ' ', person.father !== undefined ? [
-      renderPersonLink(person.father),
-      renderReference(references.father)
+      this.renderPersonLink(person.father),
+      this.renderReference(references.father)
     ] : 'None specified'
   ]};
   detailItems.push(father);
@@ -87,7 +42,7 @@ function renderCard(personKey) {
   if (person.ageOfFatherAtBirth) {
     father.kids.push(
       ' (' + person.ageOfFatherAtBirth + ' years old at birth',
-      renderReference(references.ageOfFatherAtBirth),
+      this.renderReference(references.ageOfFatherAtBirth),
       ')'
     );
   }
@@ -95,12 +50,12 @@ function renderCard(personKey) {
   var mother = { el: 'li', kids: [
     { el: 'strong', text: 'Mother:' }, ' ',
     person.mother !== undefined ? [
-      renderPersonLink(person.mother),
-      renderReference(references.mother)
+      this.renderPersonLink(person.mother),
+      this.renderReference(references.mother)
     ] : 'None specified'
   ]};
 
-  detailItems.push(mother, renderSpousesItem(personKey), renderChildrenItem(personKey))
+  detailItems.push(mother, this.renderSpousesItem(), this.renderChildrenItem())
 
 
   var card = dom({ el: 'article', class_card: true, kids: [
@@ -113,15 +68,15 @@ function renderCard(personKey) {
   ]});
   card.classList.add(person.gender);
   return card;
-}
+};
 
-function renderReference(referenceString) {
+Card.prototype.renderReference = function renderReference(referenceString) {
   var sup = dom({ el: 'sup', text: '[' + referenceString + ']' });
   return sup;
-}
+};
 
-function renderSpousesItem(personKey) {
-  var person = app.people[personKey];
+Card.prototype.renderSpousesItem = function renderSpousesItem() {
+  var person = app.people[this.personKey];
   var references = person.references;
   var spouseName = person.gender === 'male' ? 'Wife' :
     (person.gender === 'female' ? 'Husband' : 'Spouse');
@@ -140,26 +95,26 @@ function renderSpousesItem(personKey) {
   }
   else if (person.spouses.length === 1) {
     spousesItem.kids.push(
-      renderPersonLink(person.spouses[0]),
-      renderReference(references.spouses[0])
+      this.renderPersonLink(person.spouses[0]),
+      this.renderReference(references.spouses[0])
     );
   } else {
     spousesItem.kids.push(
       person.spouses.length + ' specified',
       { el: 'ul', kids: person.spouses.map(function(indexInPeople, indexInSpouses) {
         return { el: 'li', kids: [
-          renderPersonLink(indexInPeople),
-          renderReference(references.spouses[indexInSpouses])
+          this.renderPersonLink(indexInPeople),
+          this.renderReference(references.spouses[indexInSpouses])
         ]};
-      })}
+      }, this)}
     );
   }
 
   return spousesItem;
-}
+};
 
-function renderChildrenItem(personKey) {
-  var person = app.people[personKey];
+Card.prototype.renderChildrenItem = function renderChildrenItem() {
+  var person = app.people[this.personKey];
   var references = person.references;
   var numSpecified = (person.children || []).length;
 
@@ -168,7 +123,7 @@ function renderChildrenItem(personKey) {
     ' ' + (person.children ? (person.children.length + ' specified' || 'None') : 'None specified')
   ]};
   if (person.otherChildren) {
-    childrenItem.kids.push(' (some went undistinguished', renderReference(references.otherChildren), ')');
+    childrenItem.kids.push(' (some went undistinguished', this.renderReference(references.otherChildren), ')');
   }
   var ul;
   if (numSpecified > 0 || person.otherChildren) {
@@ -178,13 +133,17 @@ function renderChildrenItem(personKey) {
   if (numSpecified > 0) {
     [].push.apply(ul.kids, person.children.map(function(indexInPeople, indexInChildren) {
       return { el: 'li', kids: [
-        renderPersonLink(indexInPeople),
-        renderReference(references.children[indexInChildren])
+        this.renderPersonLink(indexInPeople),
+        this.renderReference(references.children[indexInChildren])
       ]};
-    }));
+    }, this));
   }
   return childrenItem;
-}
+};
+
+Card.prototype.renderPersonLink = function renderPersonLink(personKey) {
+  return dom({ el: 'a', text: getName(personKey), _href: '#!' + personKey });
+};
 
 function getName(personKey) {
   var person = app.people[personKey];
@@ -200,14 +159,146 @@ function getName(personKey) {
   }
 }
 
-function renderPersonLink(personKey) {
-  return dom({ el: 'a', text: getName(personKey), _href: '#!' + personKey });
-}
-
 // removes all of an element's childNodes
 function removeChilds(el) {
   var last;
   while ((last = el.lastChild)) el.removeChild(last);
+}
+
+
+
+
+function haveSameProperties(a, b) {
+  return JSON.stringify(getProperties(a)) === JSON.stringify(getProperties(b));
+}
+
+function getProperties(obj) {
+  var newObj = {};
+  for (var key in obj) {
+    newObj[key] = true;
+  }
+  return newObj;
+}
+
+//  Accepts a singular noun:
+//    getPlural('box')
+//  Returns the noun in plural form:
+//    "boxes"
+//  false positives: "ox", "potato", "goose", "deer", etc.
+function getPlural(noun) {
+  var lastChar = noun.slice(-1);
+  var last2;
+
+  // if ends with 's', 'x', 'z', 'ch', or 'sh', add 'es'
+  if ('sxz'.indexOf(lastChar) >= 0 ||
+    ['ch', 'sh'].indexOf(last2 = noun.slice(-2)) >= 0) return noun + 'es';
+
+  // else, if ends with 'y', replace with 'ies'
+  if (lastChar === 'y') return noun.slice(0, -1) + 'ies';
+
+  // else, if ends with 'fe' or 'lf', replace f+ with 'ves'
+  if (['fe', 'lf'].indexOf(last2) >= 0) {
+    var fi = noun.lastIndexOf('f');
+    return noun.slice(0, fi) + 'ves';
+  }
+
+  // else, if ends with 'man' replace with 'men'
+  if (noun.slice(-3) === 'man') return noun.slice(0, -3) + 'men';
+
+  // else add 's'
+  return noun + 's';
+}
+
+function givePossession(nounStr) {
+  return nounStr + (nounStr[nounStr.length - 1] === 's' ? '\'' : '\'s');
+}
+
+
+
+
+// init
+
+function initReftagger() {
+  // config
+  window.refTagger = {
+      settings: {
+          noSearchClassNames: ['editor-content', 'navbar']
+      }
+  };
+
+  // create <script>
+  var refTaggerScript = document.createElement('script');
+  refTaggerScript.src = '//api.reftagger.com/v2/RefTagger.js';
+
+  // append <script>
+  var firstScript = document.getElementsByTagName('script')[0];
+  firstScript.parentNode.insertBefore(refTaggerScript, firstScript);
+
+  // call refTagger.tag() whenever references are added to page
+}
+initReftagger();
+
+var handlePeople = (function() {
+  var treeParent = document.getElementById('tree');
+
+  return function handlePeople(data) {
+    window.app = data;
+    var people = app.people;
+
+    addParents(people);
+    // graphTree(getGraphData(people))
+
+    // if ( (personKey = getRoute()) ) {
+    //   displayCard(personKey);
+    // }
+
+    setupRoutes();
+  };
+})();
+handlePeople(peopleData);
+
+function addParents(people) {
+  // loop through people
+  for (var personKey in people) {
+    var parent = people[personKey];
+    var references = parent.references;
+    validatePersonData(personKey);
+    // loop through children
+    (parent.children || []).forEach(function(childKey) {
+      // add father and mother properties to each child
+      var relation = parent.gender === 'male' ? 'father' : 'mother';
+      people[childKey][relation] = personKey;
+
+      // copy parent's respective child reference as the father or mother reference for the child
+      var siblingIndex = parent.children.indexOf(childKey);
+      people[childKey].references[relation] = references.children[siblingIndex];
+    });
+  }
+
+  window.json = JSON.stringify(app, null, 2);
+  console.log('run this: copy(json)');
+}
+
+function setupRoutes() {
+  var root = null;
+  var useHash = true;
+  var router = new Navigo(root, useHash);
+
+  router
+  .on('*', function (params) {
+    var hash = location.hash.slice(1);
+    var personKey = hash.slice(1);
+    if (personKey && app.people[personKey]) {
+      new Card(personKey);
+      document.title = getName(personKey) + ' in the Bible';
+      if (refTagger.tag) refTagger.tag();
+    } else if (!personKey) {
+      location.hash = '#!adam';
+    } else {
+      console.error('Invalid person key: ' + personKey + '.');
+    }
+  })
+  .resolve();
 }
 
 function validatePersonData(personKey) {
@@ -259,159 +350,4 @@ function validatePersonData(personKey) {
       console.error(personKey + ': ' + arrayProperty + ' references do not line up');
     }
   });
-}
-
-function addParents(people) {
-  // loop through people
-  for (var personKey in people) {
-    var parent = people[personKey];
-    var references = parent.references;
-    validatePersonData(personKey);
-    // loop through children
-    (parent.children || []).forEach(function(childKey) {
-      // add father and mother properties to each child
-      var relation = parent.gender === 'male' ? 'father' : 'mother';
-      people[childKey][relation] = personKey;
-
-      // copy parent's respective child reference as the father or mother reference for the child
-      var siblingIndex = parent.children.indexOf(childKey);
-      people[childKey].references[relation] = references.children[siblingIndex];
-    });
-  }
-
-  window.json = JSON.stringify(app, null, 2);
-  console.log('run this: copy(json)');
-}
-
-function haveSameProperties(a, b) {
-  return JSON.stringify(getProperties(a)) === JSON.stringify(getProperties(b));
-}
-
-function getProperties(obj) {
-  var newObj = {};
-  for (var key in obj) {
-    newObj[key] = true;
-  }
-  return newObj;
-}
-
-//  Accepts a singular noun:
-//    getPlural('box')
-//  Returns the noun in plural form:
-//    "boxes"
-//  false positives: "ox", "potato", "goose", "deer", etc.
-function getPlural(noun) {
-  var lastChar = noun.slice(-1);
-  var last2;
-
-  // if ends with 's', 'x', 'z', 'ch', or 'sh', add 'es'
-  if ('sxz'.indexOf(lastChar) >= 0 ||
-    ['ch', 'sh'].indexOf(last2 = noun.slice(-2)) >= 0) return noun + 'es';
-
-  // else, if ends with 'y', replace with 'ies'
-  if (lastChar === 'y') return noun.slice(0, -1) + 'ies';
-
-  // else, if ends with 'fe' or 'lf', replace f+ with 'ves'
-  if (['fe', 'lf'].indexOf(last2) >= 0) {
-    var fi = noun.lastIndexOf('f');
-    return noun.slice(0, fi) + 'ves';
-  }
-
-  // else, if ends with 'man' replace with 'men'
-  if (noun.slice(-3) === 'man') return noun.slice(0, -3) + 'men';
-
-  // else add 's'
-  return noun + 's';
-}
-
-function initReftagger() {
-  // config
-  window.refTagger = {
-      settings: {
-          noSearchClassNames: ['editor-content', 'navbar']
-      }
-  };
-
-  // create <script>
-  var refTaggerScript = document.createElement('script');
-  refTaggerScript.src = '//api.reftagger.com/v2/RefTagger.js';
-
-  // append <script>
-  var firstScript = document.getElementsByTagName('script')[0];
-  firstScript.parentNode.insertBefore(refTaggerScript, firstScript);
-
-  // call refTagger.tag() whenever references are added to page
-}
-
-function getGraphData(people) {
-  var graphData = people.map(function(person, index) {
-    var obj = {
-      key: index,
-      n: person.name,
-      s: person.gender === 'male' ? 'M' : 'F'
-    };
-    if (person.mother) obj.m = person.mother;
-    if (person.father) obj.f = person.father;
-    return obj;
-  });
-  return graphData;
-}
-
-function graphTree(graphData) {
-  init();
-
-  // n: name, s: sex, m: mother, f: father, ux: wife, vir: husband, a: attributes/markers
-  setupDiagram(myDiagram, [
-    { key: 0, n: "Aaron", s: "M", m:-10, f:-11, ux: 1, a: ["C", "F", "K"] },
-    { key: 1, n: "Alice", s: "F", m:-12, f:-13, a: ["B", "H", "K"] },
-    { key: 2, n: "Bob", s: "M", m: 1, f: 0, ux: 3, a: ["C", "H", "L"] },
-    { key: 3, n: "Barbara", s: "F", a: ["C"] },
-    { key: 4, n: "Bill", s: "M", m: 1, f: 0, ux: 5, a: ["E", "H"] },
-    { key: 5, n: "Brooke", s: "F", a: ["B", "H", "L"] },
-    { key: 6, n: "Claire", s: "F", m: 1, f: 0, a: ["C"] },
-    { key: 7, n: "Carol", s: "F", m: 1, f: 0, a: ["C", "I"] },
-    { key: 8, n: "Chloe", s: "F", m: 1, f: 0, vir: 9, a: ["E"] },
-    { key: 9, n: "Chris", s: "M", a: ["B", "H"] },
-    { key: 10, n: "Ellie", s: "F", m: 3, f: 2, a: ["E", "G"] },
-    { key: 11, n: "Dan", s: "M", m: 3, f: 2, a: ["B", "J"] },
-    { key: 12, n: "Elizabeth", s: "F", vir: 13, a: ["J"] },
-    { key: 13, n: "David", s: "M", m: 5, f: 4, a: ["B", "H"] },
-    { key: 14, n: "Emma", s: "F", m: 5, f: 4, a: ["E", "G"] },
-    { key: 15, n: "Evan", s: "M", m: 8, f: 9, a: ["F", "H"] },
-    { key: 16, n: "Ethan", s: "M", m: 8, f: 9, a: ["D", "K"] },
-    { key: 17, n: "Eve", s: "F", vir: 16, a: ["B", "F", "L"] },
-    { key: 18, n: "Emily", s: "F", m: 8, f: 9 },
-    { key: 19, n: "Fred", s: "M", m: 17, f: 16, a: ["B"] },
-    { key: 20, n: "Faith", s: "F", m: 17, f: 16, a: ["L"] },
-    { key: 21, n: "Felicia", s: "F", m: 12, f: 13, a: ["H"] },
-    { key: 22, n: "Frank", s: "M", m: 12, f: 13, a: ["B", "H"] },
-
-    // "Aaron"'s ancestors
-    { key: -10, n: "Paternal Grandfather", s: "M", m: -33, f: -32, ux: -11, a: ["A", "S"] },
-    { key: -11, n: "Paternal Grandmother", s: "F", a: ["E", "S"] },
-    { key: -32, n: "Paternal Great", s: "M", ux: -33, a: ["F", "H", "S"] },
-    { key: -33, n: "Paternal Great", s: "F", a: ["S"] },
-    { key: -40, n: "Great Uncle", s: "M", m: -33, f: -32, a: ["F", "H", "S"] },
-    { key: -41, n: "Great Aunt", s: "F", m: -33, f: -32, a: ["B", "I", "S"] },
-    { key: -20, n: "Uncle", s: "M", m: -11, f: -10, a: ["A", "S"] },
-
-    // "Alice"'s ancestors
-    { key: -12, n: "Maternal Grandfather", s: "M", ux: -13, a: ["D", "L", "S"] },
-    { key: -13, n: "Maternal Grandmother", s: "F", m: -31, f: -30, a: ["H", "S"] },
-    { key: -21, n: "Aunt", s: "F", m: -13, f: -12, a: ["C", "I"] },
-    { key: -22, n: "Uncle", s: "M", ux: -21 },
-    { key: -23, n: "Cousin", s: "M", m: -21, f: -22 },
-    { key: -30, n: "Maternal Great", s: "M", ux: -31, a: ["D", "J", "S"] },
-    { key: -31, n: "Maternal Great", s: "F", m: -50, f: -51, a: ["B", "H", "L", "S"] },
-    { key: -42, n: "Great Uncle", s: "M", m: -30, f: -31, a: ["C", "J", "S"] },
-    { key: -43, n: "Great Aunt", s: "F", m: -30, f: -31, a: ["E", "G", "S"] },
-    { key: -50, n: "Maternal Great Great", s: "F", ux: -51, a: ["D", "I", "S"] },
-    { key: -51, n: "Maternal Great Great", s: "M", a: ["B", "H", "S"] }
-
-  // focus on this person
-  ], 4);
-}
-
-function givePossession(nounStr) {
-  return nounStr + (nounStr[nounStr.length - 1] === 's' ? '\'' : '\'s');
 }
